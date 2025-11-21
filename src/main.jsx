0e44e73540415d3f74559f0b0bdd487671fd8b61
MN <33024853+kokage21@users.noreply.github.com>
2025-11-22 08:22:27 +0900

    Modify pages
    Reactを修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,5 +6,14 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <RemoveLoading />
   </StrictMode>,
 )
+
+function RemoveLoading() {
+  React.useEffect(() => {
+    const loader = document.getElementById('loading-text');
+    if (loader) loader.remove();
+  }, []);
+  return null;
+}
