d78fbe71bd1ef38d41ed7000ab23efb0af767b24
MN <33024853+kokage21@users.noreply.github.com>
2025-11-22 08:31:31 +0900

    Cleanup code
    デバッグ用コードを削除

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,14 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <RemoveLoading />
   </StrictMode>,
 )
 
-function RemoveLoading() {
-  React.useEffect(() => {
-    const loader = document.getElementById('loading-text');
-    if (loader) loader.remove();
-  }, []);
-  return null;
-}
+
